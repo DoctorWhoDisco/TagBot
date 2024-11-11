e3e891ffada89d033d2c96056d632383874cef5c
DoctorWhoDisco <doctorwho.disco@gmail.com>
2024-11-11 14:54:35 +0300

    Настроила кэш и работу со временем

--- botdata.py
+++ botdata.py
@@ -1,17 +1,20 @@
 from datetime import datetime
 import math
+from functools import partial
+
 import pandas as pd
 import apscheduler
 from apscheduler.schedulers.background import BackgroundScheduler
+# from test import online
 # import time
 
 months = {"01": "Январь", "02": "Февраль", "03": "Март", "04": "Апрель", "05": "Май", "06": "Июнь",
           "07": "Июль", "08": "Август", "09": "Сентябрь", "10": "Октябрь", "11": "Ноябрь", "12": "Декабрь"}
 
-reday = {"01": "1", "02": "2", "03": "3", "04": "4", "05": "5", "06": "6",
-          "07": "7", "08": "8", "09": "9"}
+reday = {"01": 1, "02": 2, "03": 3, "04": 4, "05": 5, "06": 6,
+          "07": 7, "08": 8, "09": 9}
 
-
+on_line = []
 employ_list = []
 
 # создание класса для объекта сотрудник 1 линии
@@ -49,6 +52,7 @@ def start():
     elif datetime.strptime("00:00", "%H:%M").time() < datetime.now().time() < datetime.strptime(
             "8:59", "%H:%M").time():
         night(1)
+    time_happening()
 
 
 # Функция обработки данных из таблицы
@@ -78,19 +82,21 @@ def add_file(path):
         for elem in row[5:]:
             i += 1
             if is_not_nan(elem) and elem != "Отпуск" and elem != "Гос. Пр.":
-                active_dict.update({str(i): elem})
+                active_dict.update({i: elem})
         # Свойство active заполняется подготовленными значениями
         for emp in employ_list:
             if emp.name == row[0]:
                 emp.active = active_dict
-    print(employ_list)
+
+    print(f'{employ_list} \nData loaded')
 
 # Функция для обновления данных по времени
-def __time_happening__():
+def time_happening():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(morning(), 'cron', hour=9, minute=0)  # Каждый день в 09:00
-    scheduler.add_job(evening(), 'cron', hour=18, minute=0)
-    scheduler.add_job(night(0), 'cron', hour=21, minute=0)
+    scheduler.add_job(morning, 'cron', hour=9, minute=0)  # Каждый день в 09:00
+    scheduler.add_job(evening, 'cron', hour=18, minute=00)
+    scheduler.add_job(partial(night, 0), 'cron', hour=21, minute=00)
+    scheduler.add_job(partial(night, 1), 'cron', hour=0, minute=0)
     scheduler.start()
     print ('day_update')
 
@@ -103,6 +109,8 @@ def morning():
                 name = emp.name
                 morning_active.append(name)
     print (f'\nmorning update: {morning_active}')
+    on_line.clear()
+    on_line.extend(morning_active)
     return morning_active
 
 # Вечерний апдейт
@@ -114,6 +122,8 @@ def evening():
                 name = emp.name
                 evening_active.append(name)
     print(f'\nevening update: {evening_active}' )
+    on_line.clear()
+    on_line.extend(evening_active)
     return evening_active
 
 # Ночной апдейт
@@ -125,20 +135,25 @@ def night(i):
                 name = emp.name
                 night_active.append(name)
     print(f'\nnight update: {night_active}')
+    on_line.clear()
+    on_line.extend(night_active)
     return night_active
 
 # Функция для проверки значения на NaN
 def is_not_nan(value):
     return not (isinstance(value, float) and math.isnan(value))
 
-# это функция для определения месяца
+# Функция для определения месяца
 def month():
     today = datetime.now()
     month_name = months[today.strftime("%m")]
     return month_name
 
-# это функция для определения дня
+# Функция для определения дня
 def day():
     today = datetime.now()
-    day_name = reday[today.strftime("%d")]
+    if today.strftime("%d") in reday:
+        day_name = reday[today.strftime("%d")]
+    else:
+        day_name = int(today.strftime("%d"))
     return day_name
--- main.py
+++ main.py
@@ -1,20 +1,61 @@
+import os
+import botdata
 import telebot
 
 bot = telebot.TeleBot('5448636353:AAHOQzJrin4uvYmhKSFUwUIFd73YDtmEV6E')
+
+today = [botdata.month(), botdata.day()]
+print (today)
+botdata.start()
+print ('Данные из кэша получены')
 print ('В работе')
 
-@bot.message_handler(commands=['фдд', 'all', 'fdd'])
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+@bot.message_handler(content_types=['document'])
+def handle_docs(message):
+    try:
+        # Получаем информацию о файле
+        # Создаем путь для сохранения файла
+        file_info = bot.get_file(message.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        file_path = os.path.join(UPLOAD_DIR, message.document.file_name)
+        with open('cache.txt', 'w') as file:
+            file.write(file_path)
+        with open(file_path, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        # Проверяем, является ли файл Excel
+        if message.document.mime_type in ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                                          'application/vnd.ms-excel']:
+            botdata.add_file(file_path)
+        else:
+            bot.reply_to(message, "Пожалуйста, загрузите файл Excel.")
+    except Exception as e:
+        bot.reply_to(message, f"Произошла ошибка: {e}")
+
+@bot.message_handler(commands=['вообще_все', 'general', 'путукфд'])
 def all(message):
     try:
         text = ''
-        with open('allmem.txt', 'r') as f:
-            while True:
-                line = f.readline()
-                if not line:
+        for emp in botdata.employ_list:
+            if emp.tag != 'Уволенный сотрудник':
+                text += f'{emp.tag}, '
+        print (f'{text[:-2]}')
+        bot.send_message(message.chat.id, f'{text[:-2]}')
+    except Exception as e:
+        bot.reply_to(message, f"Произошла ошибка: {e}")
+
+@bot.message_handler(commands=['на_смене', 'онлайн', 'on_shift', 'online', 'all', 'фдд'])
+def online(message):
+    try:
+        text = ''
+        for on_name in botdata.on_line:
+            for emp in botdata.employ_list:
+                if on_name == emp.name:
+                    text += f'{emp.tag}, '
                     break
-                tag = line.split("\n")[0]
-                text += f'{tag}, '
-            #print(message)
+        print (f'{text[:-2]}')
         bot.send_message(message.chat.id, f'{text[:-2]}')
     except Exception as e:
         bot.reply_to(message, f"Произошла ошибка: {e}")
