6adde97c0461eeb8c06cc1c8a99e51a03ae6b726
DoctorWhoDisco <doctorwho.disco@gmail.com>
2024-11-12 17:50:46 +0300

    Реформатирование файла.

--- utils/botdata.py
+++ utils/botdata.py
@@ -1,7 +1,5 @@
 import math
 from datetime import datetime
-# from classcreate import add_file
-# from classupdate import group_update
 
 months = {"01": "Январь", "02": "Февраль", "03": "Март", "04": "Апрель", "05": "Май", "06": "Июнь",
           "07": "Июль", "08": "Август", "09": "Сентябрь", "10": "Октябрь", "11": "Ноябрь", "12": "Декабрь"}
